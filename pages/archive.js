4c7cddb3fd13c21fff19b47d3e0f17af12e7aa6d
smar152 <moody.butterflies@gmail.com>
2023-03-29 15:07:39 +0300

    Archive Page now links to episode beginnings, Add Season Description

--- pages/archive.js
+++ pages/archive.js
@@ -3,19 +3,35 @@ import Layout from "../Components/Layout/Layout";
 import InnerLayout from "../Components/Layout/PageInnerLayout";
 import comics from "../data/comics";
 import Head from "next/head";
+import Link from "next/link";
 
 const StEpisodes = styled("div")`
   // background-color: #fabdad;
 `;
 
+const StLink = styled(Link)`
+  color: orange;
+  text-decoration: none;
+`;
+
 const content = (
   <div>
     <StEpisodes>
-      {comics.map((season, index) => (
+      {comics.map((season, seasonIndex) => (
         <div>
-          <h2 key={index}>{season.name}</h2>
-          {season.episodes.map((episode, index) => (
-            <h3 key={index}>{episode.name}</h3>
+          <h2 key={seasonIndex}>{season.name}</h2>
+          <p>{season.description}</p>
+          {season.episodes.map((episode, episodeIndex) => (
+            <div key={episodeIndex}>
+              <StLink
+                href={`/season/${seasonIndex + 1}/episode/${
+                  episodeIndex + 1
+                }/page/${1}`}
+              >
+                <h3>{episode.name}</h3>
+              </StLink>
+              {/*<p>Description</p>*/}
+            </div>
           ))}
         </div>
       ))}
